c7a790093ab2f69188f8ff4abe2913d6bb8a189e
darigo <104947308+darighost@users.noreply.github.com>
2024-06-25 17:29:12 -0600

    Update rename.sh
    Make it actually work lol

--- rename.sh
+++ rename.sh
@@ -2,44 +2,27 @@
 
 import os
 import sys
-import fileinput
 import glob
 
-def rename_and_replace(path, old_name, new_name):
-    for entry in glob.iglob(os.path.join(path, '**', '*'), recursive=True):
-        if os.path.isdir(entry) and old_name in os.path.basename(entry):
-            # Rename folder
-            new_dir_name = os.path.basename(entry).replace(old_name, new_name)
-            os.rename(entry, os.path.join(os.path.dirname(entry), new_dir_name))
+if len(sys.argv) != 2:
+    print("Usage: python3 rename.py <new_name>")
+    sys.exit(1)
 
-        elif os.path.isfile(entry) and old_name in os.path.basename(entry):
-            # Rename file and replace content
-            with fileinput.FileInput(entry, inplace=True) as file:
-                for line in file:
-                    print(line.replace(old_name, new_name), end='')
+new_name = sys.argv[1].lower()
+files = glob.glob('desk/**/*', recursive=True)
 
-            # Rename file
-            new_file_name = os.path.basename(entry).replace(old_name, new_name)
-            os.rename(entry, os.path.join(os.path.dirname(entry), new_file_name))
-        elif os.path.isfile(entry) and os.path.basename(entry) in ['desk.docket-0', 'README.md']:
-            # Rename file and replace content
-            with fileinput.FileInput(entry, inplace=True) as file:
-                for line in file:
-                    print(line.replace(old_name, new_name), end='')
+for filename in files:
+    if not os.path.isdir(filename):
+        print(filename)
+        with open(filename, 'r+') as file:
+            contents = file.read()
+            if 'sunrise' in contents or 'Sunrise' in contents:
+                new_contents = contents.replace('sunrise', new_name)
+                new_contents = contents.replace('Sunrise', new_name.capitalize())
+                file.seek(0)
+                file.write(new_contents)
 
-if __name__ == "__main__":
-    # Check if the correct number of arguments are provided
-    if len(sys.argv) != 2:
-        print("Usage: python script.py <new_name>")
-        sys.exit(1)
+    if 'sunrise' in filename:
+        os.rename(filename, filename.replace('sunrise', new_name))
 
-    new_name = sys.argv[1]
-
-    # Set the starting path to the current directory
-    starting_path = os.getcwd()
-
-    for old_name in ["sunrise", "Sunrise"]:
-        rename_and_replace(starting_path, old_name, new_name)
-
-    print(f"Replacement complete. All instances of 'sunrise' have been replaced with '{new_name}' in files and folders.")
-
+print(f"Replacement complete. All instances of 'sunrise' have been replaced with '{new_name}' in files and folders.")
