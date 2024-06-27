4bf55f247aaf9a92abd54692348b91f3564bfd52
darigo <104947308+darighost@users.noreply.github.com>
2024-06-27 12:38:34 -0600

    Create rename.sh
    Use find command instead of Python script

---
+++ rename.sh
@@ -0,0 +1,16 @@
+#!/bin/bash
+
+NEW_NAME=$1
+
+find desk \
+  -name "*sunrise*" \
+  -exec bash -c "echo {} | sed 's/sunrise/$NEW_NAME/' | xargs mv {}" \; 2> /dev/null
+
+CAPITAL_NAME="${NEW_NAME^}"
+
+# Rename contents
+find desk \
+	-type f \
+ 	-exec sed -i '' "s/sunrise/$NEW_NAME/g" {} \; \
+  -exec sed -i '' "s/Sunrise/$CAPITAL_NAME/g" {} \;
+  
